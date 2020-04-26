c91706a1046125115fa99b3d026df67d2d69e319
D-Theriot <dylantheriot@gmail.com>
2020-04-26 10:18:00 -0500

    updated post page

--- lib/frontend/new_post_page.dart
+++ lib/frontend/new_post_page.dart
@@ -24,6 +24,8 @@ class _NewPostPageState extends State<NewPostPage> {
   File _image;
   String description;
   String handles;
+  String tagged;
+  String challenge;
 
   Future getImage(BuildContext context, bool fromCamera) async {
     print("PICKING");
@@ -117,7 +119,7 @@ class _NewPostPageState extends State<NewPostPage> {
     );
 
     AlertDialog alert = AlertDialog(
-      title: Text("How would you like to take a picture"),
+      title: Text("How would you like to take a picture?"),
       content:
           Text("Image is ready to be uploaded. How would you like to proceed?"),
       actions: [
@@ -140,87 +142,95 @@ class _NewPostPageState extends State<NewPostPage> {
   @override
   Widget build(BuildContext context) {
     TextEditingController txtCon = new TextEditingController();
+    TextEditingController tagCon = new TextEditingController();
+    TextEditingController chalCon = new TextEditingController();
     var postInfo = Provider.of<EcoPostInfo>(context);
     return SafeArea(
       child: Container(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: Card(
-                color: Constants.themeLightGreen,
-                elevation: 24,
-                child: FlatButton(
-                  onPressed: () async {
-                    File img = await showPictureSourceDialog(context);
-                    showPictureSourceDialog(context);
+        height: MediaQuery.of(context).size.height,
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Align(alignment: Alignment.center, child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Yonathan Zetune', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30,)),
+              )),
+              Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Card(
+                  color: Constants.themeLightGreen,
+                  elevation: 24,
+                  child: FlatButton(
+                    onPressed: () async {
+                      File img = await showPictureSourceDialog(context);
+                      showPictureSourceDialog(context);
 
 //                    setImageOnScreen(img);
 
-                    //Go to Leaderboard page
+                      //Go to Leaderboard page
 //                  Navigator.of(context).pushNamed('/LeaderBoardPage');
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(22.0),
-                        child: FaIcon(
-                          FontAwesomeIcons.upload,
-                          color: Colors.white,
-                          size: 26,
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(22.0),
+                          child: FaIcon(
+                            FontAwesomeIcons.upload,
+                            color: Colors.white,
+                            size: 26,
+                          ),
                         ),
-                      ),
 
-                      AutoSizeText(
-                        "Select Image",
-                        style: GoogleFonts.balooBhai(
-                            fontSize: 25, color: Colors.white),
-                      ),
+                        AutoSizeText(
+                          "Select Image",
+                          style: GoogleFonts.balooBhai(
+                              fontSize: 25, color: Colors.white),
+                        ),
 //                    FaIcon(
 //                      FontAwesomeIcons.arrowAltCircleRight,
 //                      color: Colors.white,
 //                      size: 25,
 //                    )
-                    ],
-                  ),
+                      ],
+                    ),
 //                    color: Constants.themeLightGreen,
-                  splashColor: Constants.themeGreen,
+                    splashColor: Constants.themeGreen,
+                  ),
                 ),
               ),
-            ),
-            _image == null
-                ? Text('No image selected.')
-                : SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.8,
-                height: 150,
-
-                child: Image.file(_image)),
+              _image == null
+                  ? Text('No image selected.')
+                  : SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      height: 150,
+                      child: Image.file(_image)),
 //            Text("Select an image."),
-            SizedBox(
-              height: 100,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  focusNode: FocusNode(skipTraversal: true),
-                  controller: txtCon,
-                  decoration: InputDecoration(
-                    labelText: "Enter a description!",
-                  ),
-                  keyboardType: TextInputType.text,
-                  autofocus: false,
-                  autofillHints: ["dsfsd"],
-                  onChanged: (val) {},
-                  onSubmitted: (des) async {
-                    print("SUB: " + des);
-                    setState(() {
-                      description = des;
-                    });
-                    await Requests.uploadNewPost(_image, description, "NotWiredUP");
+              Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(8.0, 4, 8, 4),
+                    child: TextField(
+                      controller: txtCon,
+                      focusNode: FocusNode(skipTraversal: true),
+                      decoration: InputDecoration(
+                        labelText: "Enter a description",
+                      ),
+                      keyboardType: TextInputType.text,
+                      autofocus: false,
+                      autofillHints: ["dsfsd"],
+                      onChanged: (val) {},
+                      onSubmitted: (des) async {
+                        print("SUB: " + des);
+                        setState(() {
+                          description = des;
+                        });
+                        await Requests.uploadNewPost(
+                            _image, description, "NotWiredUP");
 
-                    postInfo.description = des;
-                    FocusScope.of(context).unfocus();
-                  },
+                        postInfo.description = des;
+                        // FocusScope.of(context).unfocus();
+                      },
 //                onEditingComplete: (){
 //                  print("DONE");
 ////                  txtCon.
@@ -229,64 +239,163 @@ class _NewPostPageState extends State<NewPostPage> {
 //
 //
 //                },
-                ),
-              ),
-            ),
-            postInfo.description == ""
-                ? Text('')
-                : Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      postInfo.description,
-                      style: GoogleFonts.balooPaaji(
-                        fontSize: 16,
-                      ),
                     ),
                   ),
-            Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: Card(
-                color: Constants.themeLightGreen,
-                elevation: 24,
-                child: FlatButton(
-                  onPressed: () async {
-//                    showPictureSourceDialog(context);
-                    print(postInfo.selectedImg.toString());
-//                    await Requests.uploadImage(postInfo.selectedImg);
-
-                    //Go to Leaderboard page
-//                  Navigator.of(context).pushNamed('/LeaderBoardPage');
-                  },
-                  child: Row(
+                  postInfo.description == ""
+                  ? Text('')
+                  : Row(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.all(22.0),
-                        child: Icon(
-                          Icons.send,
-                          color: Colors.white,
-                          size: 26,
+                      Expanded(
+                                              child: Padding(
+                            padding: EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+                            child: Text(
+                              postInfo.description,
+                              style: GoogleFonts.balooPaaji(
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+                    child: TextField(
+                      controller: chalCon,
+                      focusNode: FocusNode(skipTraversal: true),
+                      decoration: InputDecoration(
+                        labelText: "Enter the #challenge",
+                      ),
+                      keyboardType: TextInputType.text,
+                      autofocus: false,
+                      autofillHints: ["dsfsd"],
+                      onChanged: (val) {},
+                      onSubmitted: (yeet) async {
+                        print("SUB: " + yeet);
+                        setState(() {
+                          challenge = yeet;
+                        });
+                        await Requests.uploadNewPost(_image, description, "NotWiredUP");
+                        postInfo.challenge = yeet;
+                        // FocusScope.of(context).unfocus();
+                      },
+                    ),
+                  ),
+                   postInfo.challenge == ""
+                  ? Text('')
+                  : Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Expanded(
+                                              child: Padding(
+                          padding: EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+                          child: Text(
+                            postInfo.challenge,
+                            style: GoogleFonts.balooPaaji(
+                              fontSize: 16,
+                            ),
+                          ),
                         ),
                       ),
-
-                      AutoSizeText(
-                        "Upload Post",
-                        style: GoogleFonts.balooBhai(
-                            fontSize: 25, color: Colors.white),
+                    ],
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+                    child: TextField(
+                      controller: tagCon,
+                      focusNode: FocusNode(skipTraversal: true),
+                      decoration: InputDecoration(
+                        labelText: "Tag people",
                       ),
+                      keyboardType: TextInputType.text,
+                      autofocus: false,
+                      autofillHints: ["dsfsd"],
+                      onChanged: (val) {},
+                      onSubmitted: (yeet) async {
+                        print("SUB: " + yeet);
+                        setState(() {
+                          tagged = yeet;
+                        });
+                        await Requests.uploadNewPost(_image, description, "NotWiredUP");
+                        postInfo.tagged = yeet;
+                        // FocusScope.of(context).unfocus();
+                      },
+                    ),
+                  ),
+                   postInfo.tagged == ""
+                  ? Text('')
+                  : Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Expanded(
+                                              child: Padding(
+                          padding: EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+                          child: Text(
+                            postInfo.tagged,
+                            style: GoogleFonts.balooPaaji(
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Card(
+                  color: Constants.themeLightGreen,
+                  elevation: 24,
+                  child: FlatButton(
+                    onPressed: () async {
+//                    showPictureSourceDialog(context);
+                      print(postInfo.selectedImg.toString());
+//                    await Requests.uploadImage(postInfo.selectedImg);
+
+                      //Go to Leaderboard page
+//                  Navigator.of(context).pushNamed('/LeaderBoardPage');
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(22.0),
+                          child: Icon(
+                            Icons.send,
+                            color: Colors.white,
+                            size: 26,
+                          ),
+                        ),
+
+                        AutoSizeText(
+                          "Upload Post",
+                          style: GoogleFonts.balooBhai(
+                              fontSize: 25, color: Colors.white),
+                        ),
 //                    FaIcon(
 //                      FontAwesomeIcons.arrowAltCircleRight,
 //                      color: Colors.white,
 //                      size: 25,
 //                    )
-                    ],
-                  ),
+                      ],
+                    ),
 //                    color: Constants.themeLightGreen,
-                  splashColor: Constants.themeGreen,
+                    splashColor: Constants.themeGreen,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
